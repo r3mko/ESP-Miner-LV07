15667391b2d382d6bacca0a81025fe7b94234004
Remko Kleinjan <r.a.kleinjan@gmail.com>
2025-01-26 00:37:31 +0100

    Fix global_state

--- main/vcore.c
+++ main/vcore.c
@@ -6,7 +6,6 @@
 #include "adc.h"
 #include "DS4432U.h"
 #include "TPS546.h"
-#include "global_state.h"
 
 #define TPS40305_VFB 0.6
 
@@ -22,8 +21,6 @@
 
 static const char *TAG = "vcore.c";
 
-static GlobalState * GLOBAL_STATE;
-
 esp_err_t VCORE_init(GlobalState * global_state) {
     switch (global_state->device_model) {
         case DEVICE_MAX:
@@ -37,21 +34,21 @@ esp_err_t VCORE_init(GlobalState * global_state) {
             } else {
                 ESP_RETURN_ON_ERROR(DS4432U_init(), TAG, "DS4432 init failed!");
             }
-            GLOBAL_STATE.vin = 5;
+            global_state.vin = 5;
             break;
         case DEVICE_GAMMA:
             if (TPS546_init(5) != ESP_OK) {
                 ESP_LOGE(TAG, "TPS546 init failed!");
                 return ESP_FAIL;
             }
-            GLOBAL_STATE.vin = 5;
+            global_state.vin = 5;
             break;
         case DEVICE_LV07:
             if (TPS546_init(12) != ESP_OK) {
                 ESP_LOGE(TAG, "TPS546 init failed!");
                 return ESP_FAIL;
             }
-            GLOBAL_STATE.vin = 12;
+            global_state.vin = 12;
             break;
         // case DEVICE_HEX:
         default:
