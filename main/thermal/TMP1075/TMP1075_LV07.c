401eb344f4c54a493d8ad53212ecbb3ba0ea5684
Remko Kleinjan <r.a.kleinjan@gmail.com>
2025-05-31 18:48:31 +0200

    Guard against NULL

--- main/thermal/TMP1075/TMP1075_LV07.c
+++ main/thermal/TMP1075/TMP1075_LV07.c
@@ -11,7 +11,7 @@
  */
 esp_err_t TMP1075_LV07_init(tmp1075_t *sensor, uint8_t i2c_address, const char *TAG) {
     if (sensor == NULL || TAG == NULL) {
-        ESP_LOGE(TAG, "Empty sensor or tag");
+        ESP_LOGE("TMP1075", "NULL pointer in sensor or tag");
         return ESP_FAIL;
     }
 
@@ -32,6 +32,11 @@ float TMP1075_LV07_read_temperature(tmp1075_t *sensor) {
     uint8_t data[2] = {0};
     esp_err_t err;
 
+    if (sensor == NULL) {
+        ESP_LOGE("TMP1075", "NULL pointer in sensor");
+        return NAN;
+    }
+
     err = i2c_bitaxe_register_read(sensor->dev_handle, TMP1075_LV07_TEMP_REG, data, 2);
     if (err != ESP_OK) {
         ESP_LOGE(sensor->TAG, "Failed to read temperature: %s", esp_err_to_name(err));
@@ -39,7 +44,7 @@ float TMP1075_LV07_read_temperature(tmp1075_t *sensor) {
     }
 
     // Combine MSB + upper nibble of LSB into a signed 12‐bit value:
-    //   raw12 = (data[0] << 4) | (data[1] >> 4)
+    //      raw12 = (data[0] << 4) | (data[1] >> 4)
     int16_t raw12 = ((int16_t)data[0] << 4) | ((int16_t)(data[1] >> 4) & 0x0F);
 
     // If bit 11 is set, that means a negative temperature—sign‐extend:
