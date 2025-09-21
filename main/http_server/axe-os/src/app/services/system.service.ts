790267467671b4919d14dacc7445b943484ad823
Remko Kleinjan <r.a.kleinjan@gmail.com>
2025-09-21 18:02:29 +0200

    Update mock data for temperatures

--- main/http_server/axe-os/src/app/services/system.service.ts
+++ main/http_server/axe-os/src/app/services/system.service.ts
@@ -114,6 +114,8 @@ export class SystemService {
     const hashrateData = [0,413.4903744405481,410.7764830376959,440.100549473198,430.5816012914026,452.5464981767163,414.9564271189586,498.7294609150379,411.1671601439723,491.327834852684];
     const powerData = [14.45068359375,14.86083984375,15.03173828125,15.1171875,15.1171875,15.1513671875,15.185546875,15.27099609375,15.30517578125,15.33935546875];
     const asicTempData = [-1,58.5,59.625,60.125,60.75,61.5,61.875,62.125,62.5,63];
+    const asicTemp1Data = [-1,59.5,60.625,61.125,61.75,62.5,62.875,63.125,63.5,64];
+    const asicTemp2Data = [-1,57.5,58.625,59.125,59.75,60.5,60.875,61.125,61.5,62];
     const vrTempData = [45,45,45,44,45,44,44,45,45,45];
     const asicVoltageData = [1221,1223,1219,1223,1217,1222,1221,1219,1221,1221];
     const voltageData = [5196.875,5204.6875,5196.875,5196.875,5196.875,5196.875,5196.875,5196.875,5196.875,5204.6875];
@@ -134,6 +136,8 @@ export class SystemService {
           case eChartLabel.hashrate:     statisticsList[i][j] = hashrateData[i];     break;
           case eChartLabel.power:        statisticsList[i][j] = powerData[i];        break;
           case eChartLabel.asicTemp:     statisticsList[i][j] = asicTempData[i];     break;
+          case eChartLabel.asicTemp1:    statisticsList[i][j] = asicTemp1Data[i];    break;
+          case eChartLabel.asicTemp2:    statisticsList[i][j] = asicTemp2Data[i];    break;
           case eChartLabel.vrTemp:       statisticsList[i][j] = vrTempData[i];       break;
           case eChartLabel.asicVoltage:  statisticsList[i][j] = asicVoltageData[i];  break;
           case eChartLabel.voltage:      statisticsList[i][j] = voltageData[i];      break;
