036d234e5b455c1b886c5c6c8ee0a9a3d5fe21f9
WantClue <info@wantclue.de>
2026-07-03 11:29:50 +0200

    Fix use after free race condition (#1797)
    * fix: asic use after free race condition
    
    * fix:producer-side lock
    
    * fix: make send_work hold valid_jobs_lock across free_bm_job + reassignment and valid_jobs=1 as one atomic section
    
    * fix: match the BM13xx idom

--- main/system.h
+++ main/system.h
@@ -16,7 +16,7 @@ void SYSTEM_clean_jobs_queue(GlobalState * GLOBAL_STATE);
 
 void SYSTEM_notify_accepted_share(GlobalState * GLOBAL_STATE);
 void SYSTEM_notify_rejected_share(GlobalState * GLOBAL_STATE, char * error_msg);
-void SYSTEM_notify_found_nonce(GlobalState * GLOBAL_STATE, double diff, uint8_t job_id);
+void SYSTEM_notify_found_nonce(GlobalState * GLOBAL_STATE, double diff, uint32_t target);
 void SYSTEM_notify_new_ntime(GlobalState * GLOBAL_STATE, uint32_t ntime);
 
 stratum_protocol_t stratum_protocol_from_string(const char *s);
