9f18b7db1d24594bf8231384798c3cb0dba57303
WantClue <info@wantclue.de>
2026-07-08 20:39:28 +0200

    fix: adjust freq to also fallback to default on self test (#1805)
    * fix: adjust freq to also fallback to default on self test
    
    * typo

--- main/tasks/power_management_task.c
+++ main/tasks/power_management_task.c
@@ -230,7 +230,9 @@ void POWER_MANAGEMENT_task(void * pvParameters)
         uint16_t core_voltage = GLOBAL_STATE->SELF_TEST_MODULE.is_active
                                  ? GLOBAL_STATE->DEVICE_CONFIG.family.asic.default_voltage_mv
                                  : nvs_config_get_u16(NVS_CONFIG_ASIC_VOLTAGE);
-        float asic_frequency = nvs_config_get_float(NVS_CONFIG_ASIC_FREQUENCY);
+        float asic_frequency = GLOBAL_STATE->SELF_TEST_MODULE.is_active
+                                 ? GLOBAL_STATE-> DEVICE_CONFIG.family.asic.default_frequency_mhz
+                                 : nvs_config_get_float(NVS_CONFIG_ASIC_FREQUENCY);
 
         if (core_voltage != last_core_voltage) {
             ESP_LOGI(TAG, "setting new vcore voltage to %umV", core_voltage);
